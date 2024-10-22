c4bb56c2ca6dd41c37302a80d5b973f50a3a06dd
KrishivDawra <krishivdawra@gmail.com>
2024-10-22 12:57:45 +0530

    dsds

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -11,22 +11,24 @@ function body() {
         <p className='p-5 flex justify-center items-center text-6xl font-serif font-bold'>Where Energy Meets Innovation</p>
       </div>
 
-      <h1 className='text-5xl font-serif font-bold flex justify-center items-center pb-5'>Green Energy's</h1>
+      <h1 className='text-5xl font-serif font-bold flex justify-center items-center pb-5 text-white'>Green Energy's</h1>
 
 {/* Green Energy */}
-      <div className='flex gap-6 flex-col justify-center items-center lg:flex-row'>
+      <div className='flex gap-6 flex-col justify-center items-center lg:flex-row overflow-hidden'>
 
-        <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300 '>
+        <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300    '>
           <img  src=".\src\assets\b1.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Green Energy</h1>
-          <p>Proper electrical connections ensure efficient energy transfer and prevent hazards like energy loss, system failure, or electrical fires in solar power setups.</p>
+          <p>Wind energy captures the kinetic energy of wind using turbines, converting it into electricity. This rapidly growing renewable energy source offers a sustainable alternative to fossil fuels. Wind farms, located on land or offshore, maximize energy production in high-wind areas. Wind energy significantly reduces greenhouse gas emissions and creates jobs in manufacturing and maintenance, contributing to energy independence.
+          </p>
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
           <img src=".\src\assets\manf.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Nitin Gadhkari</h1>
-          <p>The manufacturing industry drives innovation, economic growth, and provides essential products worldwide.</p>
-        </div>
+          <p>Solar energy harnesses sunlight through photovoltaic cells to generate clean electricity. This renewable resource reduces reliance on fossil fuels and lowers carbon emissions. Solar panels can be installed on rooftops or in solar farms, providing cost savings on electricity bills. Continuous advancements in solar technology enhance efficiency and affordability, making it an increasingly popular option for individuals and businesses seeking sustainable energy solutions.
+          </p>
+          </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
           <img src=".\src\assets\manf.jpg" alt="" />
@@ -58,7 +60,7 @@ function body() {
 
 {/* EV's */}
 
-      <h1 className='pt-8 text-5xl font-serif font-bold flex justify-center items-center pb-5'>EV's</h1>
+      <h1 className='pt-8 text-5xl font-serif font-bold flex justify-center items-center pb-5 text-white'>EV's</h1>
 
       <div className='flex gap-6 flex-col justify-center items-center'>
 
