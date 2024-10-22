ae5b9222f095037d33328ad18f32036ac54ac18e
Hardik <109456477+hardik6301@users.noreply.github.com>
2024-10-22 14:05:20 +0530

    +

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -32,7 +32,7 @@ function body() {
           </div>
 
         <div className='w-80 h-128 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\geo 1.jpeg" alt="" />
+          <img className='h-70' src=".\src\assets\geo 1.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>HydroPower Energy</h1>
           <p>Hydropower generates electricity by utilizing the energy of flowing or falling water. It is one of the oldest renewable energy sources and provides reliable, consistent power. Dams or run-of-the-river systems convert gravitational energy into electrical energy, offering flexibility for peak demand management. While hydropower reduces carbon emissions, it's vital to address ecological impacts and ensure sustainable water resource management to protect local ecosystems.</p>
         </div>
@@ -41,19 +41,19 @@ function body() {
       <div className='lg:pt-5 lg:flex lf:gap-6  lg:justify-center lg:gap-5 lg:items-center hidden'>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\geo 2.jpeg" alt="" />
+          <img className='h-70' src=".\src\assets\geo 2.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Geothermal Energy</h1>
           <p>Geothermal energy harnesses heat from the Earth's interior for electricity generation and heating applications. By tapping into hot water reservoirs beneath the surface, geothermal power plants provide a stable and reliable energy supply. This renewable source has low environmental impact and significantly reduces greenhouse gas emissions. Although initial investment costs can be high, geothermal systems offer long-term savings and sustainability benefits, contributing to a diversified energy portfolio.</p>
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\geo 3.jpeg" alt="" />
+          <img className='h-70' src=".\src\assets\geo 3.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Energy Efficiency</h1>
           <p>Energy efficiency involves using less energy to achieve the same results, leading to reduced consumption and costs. Improving energy efficiency is crucial for combating climate change and lowering greenhouse gas emissions. Strategies include upgrading appliances, enhancing building insulation, and adopting smart technologies. By prioritizing energy-efficient practices, individuals, businesses, and governments can save money on energy bills while contributing to a more sustainable future and reducing environmental impact.</p>
         </div>
 
         <div className=' w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\solar.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\solar.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Solar Energy</h1>
           <p>Solar energy harnesses sunlight through photovoltaic cells to generate clean electricity. This renewable resource reduces reliance on fossil fuels and lowers carbon emissions. Solar panels can be installed on rooftops or in solar farms, providing cost savings on electricity bills. Continuous advancements in solar technology enhance efficiency and affordability, making it an increasingly popular option for individuals and businesses seeking sustainable energy solutions.
           </p>
@@ -67,7 +67,7 @@ function body() {
       <div className='flex gap-6 flex-col justify-center items-center lg:flex-row'>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\solar.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\ev 1.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Environmental Benefits
           </h1>
           <p>Electric vehicles (EVs) significantly reduce greenhouse gas emissions compared to traditional gasoline or diesel vehicles. By producing zero tailpipe emissions, EVs help improve air quality, contributing to a cleaner environment. As the electricity grid transitions to renewable energy sources, the overall carbon footprint of EVs will continue to decrease. Embracing electric vehicles is a crucial step toward combating climate change and promoting sustainable transportation solutions.
@@ -75,14 +75,14 @@ function body() {
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\ev 5.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Cost Savings</h1>
           <p>While the upfront cost of electric vehicles can be higher than conventional vehicles, they offer significant long-term savings. EVs have lower operating costs due to reduced fuel expenses and less maintenance, as they have fewer moving parts. Many governments provide incentives and rebates for EV purchases, making them more financially accessible. Over time, the total cost of ownership for electric vehicles can be significantly lower than that of traditional vehicles.
           </p>
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\ev 3.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Advancements in Technology
           </h1>
           <p>Recent advancements in battery technology have led to increased energy density, longer driving ranges, and faster charging times for electric vehicles. Innovations such as solid-state batteries and improved lithium-ion technologies are making EVs more efficient and convenient for everyday use. Furthermore, the development of smart charging solutions and vehicle-to-grid technology enhances the overall functionality of electric vehicles, promoting a more integrated energy ecosystem.
@@ -93,7 +93,7 @@ function body() {
       <div className='lg:pt-5 lg:flex lf:gap-6  lg:justify-center lg:gap-5 lg:items-center hidden'>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\solar.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\ev 4.jpeg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Charging Infrastructure
           </h1>
           <p>The growth of electric vehicle adoption is supported by the expanding charging infrastructure across urban and rural areas. Public charging stations, home charging solutions, and fast-charging networks are being developed to make EV charging more accessible and convenient. As more charging points become available, range anxiety diminishes, encouraging more drivers to transition to electric vehicles. Collaborations between governments and private companies are essential in building this crucial infrastructure.
@@ -101,7 +101,7 @@ function body() {
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\ev 6.png" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Increased Consumer Choice
           </h1>
           <p>The electric vehicle market has expanded significantly, offering consumers a diverse range of options across various segments. From compact cars to SUVs and luxury models, manufacturers are introducing electric variants to meet consumer demand. This increased choice allows buyers to select vehicles that fit their lifestyle and preferences while contributing to sustainability goals. As more manufacturers invest in EV development, consumers can expect even more innovative options in the future.
@@ -109,7 +109,7 @@ function body() {
         </div>
 
         <div className=' w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\manf.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Future of Transportation
           </h1>
           <p>The transition to electric vehicles is reshaping the future of transportation, moving toward sustainable mobility solutions. Governments worldwide are implementing policies to phase out internal combustion engine vehicles, promoting electric alternatives as part of their climate action plans. Autonomous electric vehicles also hold the potential to revolutionize transportation, providing safer, more efficient mobility options. As technology continues to evolve, the future of transportation will increasingly rely on electric vehicles.
@@ -125,7 +125,7 @@ function body() {
       <div className='flex gap-6 flex-col justify-center items-center lg:flex-row'>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\im1.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi 1.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Role in Economic Growth
           </h1>
           <p>The manufacturing industry is a key driver of economic growth, creating jobs, boosting exports, and fostering innovation. By producing goods for domestic and global markets, manufacturers support infrastructure development and enhance productivity. Manufacturing contributes to wealth generation and the overall economic stability of countries, playing a crucial role in shaping industrialized and developing economies alike.
@@ -133,7 +133,7 @@ function body() {
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi 2.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Technological Advancements
           </h1>
           <p>Automation, robotics, and artificial intelligence (AI) are revolutionizing the manufacturing industry, making production processes more efficient and cost-effective. Advanced technologies streamline operations, reduce human error, and improve product quality. With the rise of Industry 4.0, manufacturers are adopting smart technologies to optimize supply chains, minimize waste, and enhance decision-making through real-time data analysis, transforming traditional manufacturing models.
@@ -141,7 +141,7 @@ function body() {
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi 3.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Sustainable Manufacturing</h1>
           <p>Sustainable practices in the manufacturing industry are becoming increasingly important, with a focus on reducing waste, energy consumption, and emissions. By implementing eco-friendly technologies and recycling programs, manufacturers can minimize their environmental impact. Sustainable manufacturing also enhances brand reputation, attracts eco-conscious consumers, and supports long-term profitability while promoting environmental responsibility within the industry.
           </p>
@@ -151,14 +151,14 @@ function body() {
       <div className='lg:pt-5 lg:flex lf:gap-6  lg:justify-center lg:gap-5 lg:items-center hidden'>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\solar.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi 4.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Global Supply Chain Integration</h1>
           <p>The manufacturing industry plays a critical role in global supply chains, ensuring the efficient production and delivery of goods worldwide. Manufacturers work with suppliers and distributors to optimize operations and reduce lead times. However, global disruptions, such as the COVID-19 pandemic, have highlighted the need for resilience and flexibility in supply chains, encouraging businesses to diversify sourcing and explore local manufacturing options.
           .</p>
         </div>
 
         <div className='w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi5.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'>Workforce Development
           </h1>
           <p>As manufacturing becomes more automated and technologically advanced, the demand for skilled workers increases. Manufacturers must invest in workforce development through training and education to equip employees with the skills needed for modern manufacturing environments. This includes expertise in operating advanced machinery, understanding data analytics, and maintaining automation systems, ensuring the industry's workforce is ready for future challenges.
@@ -166,7 +166,7 @@ function body() {
         </div>
 
         <div className=' w-80 border-solid border-black border-2 rounded-lg bg-slate-300'>
-          <img src=".\src\assets\manf.jpg" alt="" />
+          <img className='h-70' src=".\src\assets\mi 6.jpg" alt="" />
           <h1 className='justify-center items-center flex font-bold font-serif text-2xl'> Customization and Flexibility
           </h1>
           <p>Modern manufacturing trends emphasize customization and flexibility to meet changing consumer demands. Companies are shifting from mass production to personalized, small-batch manufacturing, enabled by technologies like 3D printing and digital design tools. This shift allows manufacturers to respond quickly to market trends, enhance customer satisfaction, and reduce excess inventory, driving greater efficiency and profitability in a dynamic global marketplace.
