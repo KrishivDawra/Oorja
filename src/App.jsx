b4eac82b7553a9185d2ddada71c08f565f8c2ee5
kartik941 <kk6734401@gmail.com>
2024-10-22 14:33:57 +0530

    Revert "all"
    This reverts commit 8a771b9b5c669eb04cbe68884b07dd2bc5d5af00.

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,8 @@ function App() {
   return (
     <>
     <div className="bg-seagreen scroll-smooth">
-    <div className='fixed w-full'>
-    <Header/></div>
-    <div><img className="lg:flex lg:m-auto lg:h-screen lg:w-full" src=".\src\assets\hero img.jpeg" alt="" /></div>
+    <Header/>
+    <div><img className="lg:flex lg:m-auto lg:h-screen" src=".\src\assets\hero img.jpeg" alt="" /></div>
     <Body/>
     <Footer/>
     </div>
