2e7ce82ec6e6c2492cca25747a1804fd8a865a6a
kartik941 <kk6734401@gmail.com>
2024-10-22 13:57:55 +0530

    done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ function header() {
    <div className='flex justify-between bg-stone-900 align-baseline text-slate-50 '>
       <img className='h-[60px] w-[150px]  mt-4 size-48 ' src=".\src\assets\header.png" alt="" />
         <ul className='lg:flex lg:space-x-4 lg:py-8 lg:text-slate-50 hidden '>
-            <li className='font-serif font-semibold  hover:text-orange-500 cursor-pointer' > Home</li>
+            <li className='font-serif font-semibold  hover:text-orange-500 cursor-pointer'  > Home</li>
             <li className='font-serif font-semibold  hover:text-orange-500 cursor-pointer'> Green Energy</li>
             <li className='font-serif font-semibold  hover:text-orange-500 cursor-pointer'>EV's Sector</li>
             <li className='font-serif font-semibold  hover:text-orange-500 cursor-pointer'> manufaction & infrastructure</li>
